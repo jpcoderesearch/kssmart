d4654ae421385b2d69b7707d645642342ab02570
jpcoderesearch <jpcoderesearch@gmail.com>
2017-07-15 21:02:25 +0530

    route set

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :home_tests
-  root "home_tests_controller#index"
+  root "home_tests#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
