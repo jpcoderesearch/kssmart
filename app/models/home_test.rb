7b1575bcdc835da1aaae8eeeb215845882ea50a5
jpcoderesearch <jpcoderesearch@gmail.com>
2017-07-20 09:26:36 +0530

    change file name

--- app/models/home_test.rb
+++ app/models/home_test.rb
@@ -1,6 +1,7 @@
 class HomeTest < ApplicationRecord
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
+	path: ":rails_root/assets/images/:style/:basename.:extension",
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
